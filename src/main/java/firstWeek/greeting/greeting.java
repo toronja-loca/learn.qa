24232e08dc09efbe23b1dda9109d60fcd61d46d0
Найля Уталиева <nelly55037@gmail.com>
2021-12-17 19:08:46 +0300

    greeting task changed

--- src/main/java/firstWeek/greeting/greeting.java
+++ src/main/java/firstWeek/greeting/greeting.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class greeting {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         greeting("23", "50", "45", "Carlos");
     }
 
