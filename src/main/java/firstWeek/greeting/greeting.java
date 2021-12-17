1a4c9d6c67efcadbf7993dabf20967bc9d8903dd
Найля Уталиева <nelly55037@gmail.com>
2021-12-17 11:47:15 +0300

    greeting task changed

--- src/main/java/firstWeek/greeting/greeting.java
+++ src/main/java/firstWeek/greeting/greeting.java
@@ -1,18 +1,30 @@
 package firstWeek.greeting;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class greeting {
     public static void main(String[] args) {
-        greeting(24, "Carlos");
+        greeting("23", "50", "45", "Carlos");
     }
 
-    public static void greeting(int hours, String name){
-        if(hours > 5 & hours < 12){
+    public static void greeting(String hours, String minutes, String seconds, String name) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String setTime = hours + ":" + minutes + ":" + seconds;
+        Date currentTime = sdf.parse(setTime);
+        Date morning = sdf.parse("05:00:00");
+        Date day = sdf.parse("12:00:00");
+        Date evening = sdf.parse("16:00:00");
+        Date night = sdf.parse("23:00:00");
+
+        if(currentTime.after(morning) & currentTime.before(day)){
             System.out.printf("Доброе утро, %s", name);
         }
-        else if(hours > 12 & hours < 16){
+        else if(currentTime.after(day) & currentTime.before(evening)){
             System.out.printf("Добрый день, %s", name);
         }
-        else if(hours >16 & hours < 23){
+        else if(currentTime.after(evening) & currentTime.before(night)){
             System.out.printf("Добрый вечер, %s", name);
         }
         else{
